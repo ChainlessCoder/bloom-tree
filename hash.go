4fb2ae8ab27e33367cd38b2e140ddb45c6bc661b
Lum Ramabaja <lumramabaja@gmail.com>
2020-01-22 14:36:23 +0100

    Update hash.go

--- hash.go
+++ hash.go
@@ -28,24 +28,3 @@ func hashLeaf(element, index uint64) [sha512.Size256]byte {
 func chunkSize() int {
 	return 64
 }
-
-
-// These methods are temporary. Only for testing things
-
-func P2(elem1, elem2 [32]byte) [32]byte {
-	var elem []byte
-	elem = append(elem, elem1[:]...)
-	elem = append(elem, elem2[:]...)
-	return sha512.Sum512_256(elem)
-}
-
-func P1(element, index uint64) [sha512.Size256]byte {
-	var elem []byte
-	a := make([]byte, chunkSize())
-	binary.LittleEndian.PutUint64(a, index)
-	b := make([]byte, chunkSize())
-	binary.LittleEndian.PutUint64(b, element)
-	elem = append(elem, a[:]...)
-	elem = append(elem, b[:]...)
-	return sha512.Sum512_256(elem)
-}
