cd7b5547a98fc4b076af7be85c7514383254ae11
Arber Avdullahu <arber_919@live.com>
2020-01-24 21:09:05 +0100

    proof tests

--- proof_test.go
+++ proof_test.go
@@ -1,6 +1,7 @@
 package bloomtree
 
 import (
+	"errors"
 	"testing"
 )
 
@@ -46,6 +47,150 @@ func TestPresenceProofPresentElement(t *testing.T) {
 			t.Fatal(err)
 		} else if !present {
 			t.Fatal("expected element to be present, but is absent")
+		}
+	}
+}
+
+func TestPresenceProofAbsentElement(t *testing.T) {
+	var tests = []struct {
+		element      []byte
+		elementProof []byte
+		elements     [][]byte
+	}{
+		{
+			element:      []byte{9},
+			elementProof: []byte{8},
+			elements:     [][]byte{{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}},
+		},
+		{
+			element:      []byte{17},
+			elementProof: []byte{16},
+			elements: [][]byte{{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13},
+				{14}, {15}, {16}},
+		},
+		{
+			element:      []byte{2},
+			elementProof: []byte{0},
+			elements:     [][]byte{{0}, {1}},
+		},
+	}
+
+	for _, test := range tests {
+		seed := "secret seed"
+		dbf := generateDBF(seed, test.elements...)
+		tree, err := NewBloomTree(dbf)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		multiproof, err := tree.GenerateCompactMultiProof(test.elementProof)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if CheckProofType(multiproof.proofType) != true {
+			t.Fatal("proof type is not present")
+		}
+
+		_, err = tree.VerifyCompactMultiProof(test.element, []byte(seed), multiproof, tree.Root())
+		if err == nil {
+			t.Fatalf("expected error: %v", errors.New("The element is not inside the provided chunks for a presence proof"))
+		} else if err.Error() != errors.New("The element is not inside the provided chunks for a presence proof").Error() {
+			t.Fatalf("expected error %v, but got %v", errors.New("The element is not inside the provided chunks for a presence proof"), err)
+		}
+	}
+}
+
+func TestAbsentProofAbsentElement(t *testing.T) {
+	var tests = []struct {
+		element  []byte
+		elements [][]byte
+	}{
+		{
+			element:  []byte{9},
+			elements: [][]byte{{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}},
+		},
+		{
+			element: []byte{17},
+			elements: [][]byte{{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13},
+				{14}, {15}, {16}},
+		},
+		{
+			element:  []byte{2},
+			elements: [][]byte{{0}, {1}},
+		},
+	}
+
+	for _, test := range tests {
+		seed := "secret seed"
+		dbf := generateDBF(seed, test.elements...)
+		tree, err := NewBloomTree(dbf)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		multiproof, err := tree.GenerateCompactMultiProof(test.element)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if CheckProofType(multiproof.proofType) != false {
+			t.Fatal("proof type is not absent")
+		}
+
+		absent, err := tree.VerifyCompactMultiProof(test.element, []byte(seed), multiproof, tree.Root())
+		if err != nil {
+			t.Fatal(err)
+		} else if !absent {
+			t.Fatal("expected element to be absent, but is present")
+		}
+	}
+}
+
+func TestAbsenceProofPresentElement(t *testing.T) {
+	var tests = []struct {
+		element      []byte
+		elementProof []byte
+		elements     [][]byte
+	}{
+		{
+			element:      []byte{8},
+			elementProof: []byte{9},
+			elements:     [][]byte{{1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}},
+		},
+		{
+			element:      []byte{16},
+			elementProof: []byte{17},
+			elements: [][]byte{{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13},
+				{14}, {15}, {16}},
+		},
+		{
+			element:      []byte{0},
+			elementProof: []byte{2},
+			elements:     [][]byte{{0}, {1}},
+		},
+	}
+
+	for _, test := range tests {
+		seed := "secret seed"
+		dbf := generateDBF(seed, test.elements...)
+		tree, err := NewBloomTree(dbf)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		multiproof, err := tree.GenerateCompactMultiProof(test.elementProof)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if CheckProofType(multiproof.proofType) != false {
+			t.Fatal("proof type is not absent")
+		}
+
+		_, err = tree.VerifyCompactMultiProof(test.element, []byte(seed), multiproof, tree.Root())
+		if err != nil {
+			t.Fatal(err)
 		}
 	}
 }
